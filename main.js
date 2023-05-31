2a0e7ff082adaf6824890918e1283e2d4bba4598
neha221109 <117840055+neha221109@users.noreply.github.com>
2023-05-31 20:43:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function submit() {
+student1=document.getElementById("student1").value;
+students=[];
+students.push(student1)
+student2=document.getElementById("student2").value;
+students.push(student2)
+student3.document.getElementById("student3").value;
+students.push(student3)
+student4.document.getElementById("student4").value;
+students.push(student4);
+document.getElementById("studentlist").innerHTML=students
+}
